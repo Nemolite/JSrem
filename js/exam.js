3d8f221b36dfee13e6b6d100928debe2e14b24ac
Nemolite <g25092011@mail.ru>
2020-05-14 08:43:00 +0300

    jsrem2

--- js/exam.js
+++ js/exam.js
@@ -3,5 +3,17 @@ console.log(111)
 var element = document.getElementById('test');
 console.log(element)
 
+/*exam 2 */
 
+var x = document.getElementById("test");
+var temx = x.parentNode.nodeName;
+
+console.log(temx)
+
+
+
+
+
+var y = document.getElementsByTagName("li")[0];
+ 
 
