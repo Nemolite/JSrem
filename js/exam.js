4485b4ede346dfe63a233227f7cf16d2a496d16c
Nemolite <g25092011@mail.ru>
2020-05-14 15:01:01 +0300

    jsrem3

--- js/exam.js
+++ js/exam.js
@@ -5,15 +5,32 @@ console.log(element)
 
 /*exam 2 */
 
+
+/* Этот код находит в документе объект с 
+id = test определяет кто его родитель
+и выводит его к консоль*/
 var x = document.getElementById("test");
 var temx = x.parentNode.nodeName;
+console.log(temx)  /*output=>BODY */
 
-console.log(temx)
+/*Еще один такой же пример 
+Тут мы находим 2 элемент с тегом p 
+выводим его на экран и
+определяем родителя
 
+*/
 
+var y = document.getElementsByTagName("p")[1];
+console.log(y) 
+var temy = y.parentNode.nodeName;
+console.log(temy) /*output=>BODY */
 
+/*другой пример */
 
+var h = document.getElementById("inner-h1");
+console.log(h) 
+var temh1 = h.parentNode.nodeName;
+console.log(temh1) /*output=>DIV */
 
-var y = document.getElementsByTagName("li")[0];
- 
-
+var currentElem = document.getElementById("next-inner");
+currentElem.parentNode.style.display = "none";
